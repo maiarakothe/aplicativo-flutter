7134f45a0445c11bbdf83e6740cd60a017941491
Maiara Kothe <maiarakothe0@gmail.com>
2025-03-27 14:41:01 -0300

    Update product_list.dart

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,52 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ShowProductPage extends StatelessWidget {
-  final List<Map<String, String>> produtos;
-  final void Function(int) onEdit;
-  final void Function(int) onDelete;
+  final List<Map<String, String>> products;
+  final Function(int) onEdit;
+  final Function(int) onDelete;
 
-  ShowProductPage({required this.produtos, required this.onEdit, required this.onDelete});
+  ShowProductPage({required this.products, required this.onEdit, required this.onDelete});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: produtos.isEmpty
-          ? Center(
-        child: Text(
-          AppLocalizations.of(context).noProducts,
-          style: TextStyle(fontSize: 18),
-        ),
-      )
-          : ListView.builder(
-        itemCount: produtos.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Image.network(produtos[index]['imagem']!),
-            title: Text(produtos[index]['nome']!),
-            subtitle: Text('${AppLocalizations.of(context).price}: R\$ ${produtos[index]['preco']}'),
+    return ListView.builder(
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return Card(
+          child: ListTile(
+            leading: Image.network(
+              product['image']!,
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+            title: Text(product['name']!),
+            subtitle: Text("Preço: R\$ ${product['price']}"),
             trailing: Row(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Tooltip(
-                  message: AppLocalizations.of(context).edit,
-                  child: IconButton(
-                    icon: Icon(Icons.edit, color: Colors.blue),
-                    onPressed: () => onEdit(index),
-                  ),
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () => onEdit(index),
                 ),
-                Tooltip(
-                  message: AppLocalizations.of(context).delete,
-                  child: IconButton(
-                    icon: Icon(Icons.delete, color: Colors.red),
-                    onPressed: () => onDelete(index),
-                  ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () => onDelete(index),
                 ),
               ],
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
